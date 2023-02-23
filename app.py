0ae24d39bd8fa885cc6d534cbe9f4b0395c70b27
Fredh Macau <80861318+fredhmacau@users.noreply.github.com>
2023-02-23 21:40:10 +0100

    Revert "Revert "Update app.py""
    This reverts commit f07f3e072b61fe11cf8c0c0d69e7fbaa3043d088.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 #http://127.0.0.1:5000/ 
-#http://127.0.0.1:5000/api/v1/movies
+#http://127.0.0.1:5000/api/v1/movie
 app.register_blueprint(get.get_movie)
 
 #http://127.0.0.1:5000/api/v1/movie data={name,casts,genres}
